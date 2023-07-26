6b9b8079f689ddb3abbe69acc517bc7124ed8c4b
Liam2263 <65407720+Liam2263@users.noreply.github.com>
2023-07-25 20:23:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-var MainRoomController = require('main');
+var MainRoomController = require('RoomController');
 
 module.exports.loop = function () {
     all_owned_room_names = []
-    console.log('test');
     for (roomName in Game.rooms)
     {
         const room = Game.rooms[roomName];
-        owner_name = room.controller.owner;
-        console.log('test');
+        owner_name = room.controller.owner.username;
         if(owner_name == 'SoldierDoge')
         {
             var targets = room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN;
+                        return (structure.structureType == STRUCTURE_SPAWN);
                     }
             });
-            var spawn = targets[0];
             
-            MainRoomController.run(room, spawn);
+            if(targets.length == 0)
+            {
+                var spawn = "null";
+                
+                MainRoomController.run(room, spawn);
+            }
+            else
+            {
+                var spawn = targets[0];
+                
+                MainRoomController.run(room, spawn);
+            }
         }
     }
 }
